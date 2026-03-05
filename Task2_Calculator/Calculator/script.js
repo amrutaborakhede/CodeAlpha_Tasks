a990bd00bcf9c8fc208cf15bf3241567aa8098a1
amrutaborakhede <amrutaborakhede@gmail.com>
2026-03-05 11:10:31 +0530

    Add files via upload

---
+++ Task2_Calculator/Calculator/script.js
@@ -0,0 +1,43 @@
+const display = document.getElementById("display");
+
+// Add value to display
+function appendValue(value) {
+  display.value += value;
+}
+
+// Clear display
+function clearDisplay() {
+  display.value = "";
+}
+
+// Delete last character
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+// Calculate result
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
+
+// Keyboard Support (Bonus)
+document.addEventListener("keydown", function (event) {
+  const key = event.key;
+
+  if (!isNaN(key) || "+-*/.".includes(key)) {
+    appendValue(key);
+  }
+  else if (key === "Enter") {
+    calculate();
+  }
+  else if (key === "Backspace") {
+    deleteLast();
+  }
+  else if (key === "Escape") {
+    clearDisplay();
+  }
+});
